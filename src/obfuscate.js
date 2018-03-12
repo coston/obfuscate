40b95bd82c2a4687b6a8db37c4f1c5855b54a546
Coston Perkins <coston.perkins@ua.edu>
2018-03-12 09:39:41 -0500

    format code

--- src/obfuscate.js
+++ src/obfuscate.js
@@ -1,8 +1,8 @@
-import assert from 'assert-ok';
-import array from 'cast-array';
-import filter from 'object-filter';
-import qs from 'query-string';
-import bel from 'bel';
+import assert from 'assert-ok'
+import array from 'cast-array'
+import filter from 'object-filter'
+import qs from 'query-string'
+import bel from 'bel'
 
 export default function obfuscate(options) {
   assert(options, 'options are required')
@@ -12,8 +12,8 @@ export default function obfuscate(options) {
     cc: addresses(options.cc),
     bcc: addresses(options.bcc),
     subject: options.subject,
-    body: options.body
-  };
+    body: options.body,
+  }
 
   const styles = `
     unicode-bidi: bidi-override; 
@@ -21,39 +21,36 @@ export default function obfuscate(options) {
     unicode-bidi: -moz-isolate-override; 
     unicode-bidi: -ms-isolate-override; 
     unicode-bidi: isolate-override;
-  `;
+  `
 
-  const email = mailto.email;
+  const email = mailto.email
   mailto = filter(mailto, Boolean)
   delete mailto.email
 
-  const querystring = qs.stringify(mailto);
+  const querystring = qs.stringify(mailto)
 
-  const link = `mailto:${email || ''}${querystring
-    ? '?' + querystring
-    : ''}`;
-  return bel `<a dir="rtl" style=${styles} onclick="${ () => {
-    handleClick(event, link)}}" href="obfuscated">${reverse(email)}</a>`
-  };
+  const link = `mailto:${email || ''}${querystring ? '?' + querystring : ''}`
+  return bel`<a dir="rtl" style=${styles} onclick="${() => {
+    handleClick(event, link)
+  }}" href="obfuscated">${reverse(email)}</a>`
+}
 
-  function addresses(e) {
-    return e
-      ? array(e).join(',')
-      : undefined
-  }
+function addresses(e) {
+  return e ? array(e).join(',') : undefined
+}
 
-  function reverse(s) {
-    return s
-      .split("")
-      .reverse()
-      .join("");
-  }
+function reverse(s) {
+  return s
+    .split('')
+    .reverse()
+    .join('')
+}
 
-  function wait(e) {
-    e.preventDefault();
-  }
+function wait(e) {
+  e.preventDefault()
+}
 
-  function handleClick(e, l) {
-    e.preventDefault()
-    window.location.href = l
-  }
+function handleClick(e, l) {
+  e.preventDefault()
+  window.location.href = l
+}
