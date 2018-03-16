15367995e5aa6ea5cbc00b87211063b03254b68e
Coston Perkins <coston.perkins@ua.edu>
2018-03-15 21:59:35 -0500

    format code

--- src/obfuscate.js
+++ src/obfuscate.js
@@ -14,7 +14,7 @@ module.exports = function obfuscate(options) {
     cc: addresses(options.cc),
     bcc: addresses(options.bcc),
     subject: options.subject,
-    body: options.body
+    body: options.body,
   }
 
   var email = mailto.email
@@ -23,32 +23,29 @@ module.exports = function obfuscate(options) {
 
   var querystring = qs.stringify(mailto)
 
-  var link = 'mailto:' + (email || '') + (querystring
-    ? '?' + querystring
-    : '')
+  var link = 'mailto:' + (email || '') + (querystring ? '?' + querystring : '')
 
-  return bel `<a style="direction: rtl; unicode-bidi: bidi-override;" onclick="${ function () {
-    handleClick(event, link)}}" href="obfuscated">${reverse(email)}</a>`
-  }
+  return bel`<a style="direction: rtl; unicode-bidi: bidi-override;" onclick="${function() {
+    handleClick(event, link)
+  }}" href="obfuscated">${reverse(email)}</a>`
+}
 
-  function addresses(e) {
-    return e
-      ? array(e).join(',')
-      : undefined
-  }
+function addresses(e) {
+  return e ? array(e).join(',') : undefined
+}
 
-  function reverse(s) {
-    return s
-      .split("")
-      .reverse()
-      .join("");
-  }
+function reverse(s) {
+  return s
+    .split('')
+    .reverse()
+    .join('')
+}
 
-  function wait(e) {
-    e.preventDefault();
-  }
+function wait(e) {
+  e.preventDefault()
+}
 
-  function handleClick(e, l) {
-    e.preventDefault()
-    window.location.href = l
-  }
+function handleClick(e, l) {
+  e.preventDefault()
+  window.location.href = l
+}
